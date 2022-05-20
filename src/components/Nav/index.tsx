602e012531860b6b2357a302ce4fe51f1806d103
鲁举佩 <653390161@qq.com>
2022-05-20 21:26:32 +0800

    refactor(nav): remove timeline and category item

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -16,18 +16,18 @@ const routes = [
 		path: '/blog',
 		label: '文章'
 	},
-	{
-		path: '/category',
-		label: '分类'
-	},
+	// {
+	// 	path: '/category',
+	// 	label: '分类'
+	// },
 	{
 		path: '/leave-message',
 		label: '留言'
 	},
-	{
-		path: '/timeline',
-		label: '时间轴'
-	},
+	// {
+	// 	path: '/timeline',
+	// 	label: '时间轴'
+	// },
 	{
 		path: '/about-me',
 		label: '关于我'
